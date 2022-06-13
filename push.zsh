f7605d20f33cd13c063c55f2395640b4c17209f2
curlynux <curlynux@gmail.com>
2022-06-13 16:26:35 +0200

    first push

---
+++ push.zsh
@@ -0,0 +1,7 @@
+echo # curlynux.github.io-P04_samory_toure_original >> README.md
+git init
+git add README.md
+git commit -m first commit
+git branch -M main
+git remote add origin https://github.com/curlynux/curlynux.github.io-P04_samory_toure_original.git
+git push -u origin main
